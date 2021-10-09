781bf17c31fb437ce2087ed0c791f79658b14706
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-09 11:02:46 +0200

    Betty fixed

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -7,14 +7,14 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *n;
+	binary_tree_t *n;
 
-    n = malloc(sizeof(binary_tree_t));
-    if (n == NULL)
-        return (NULL);
-    n->parent = parent;
-    n->n = value;
-    n->right = NULL;
-    n->left = NULL;
-    return (n);
+	n = malloc(sizeof(binary_tree_t));
+	if (n == NULL)
+		return (NULL);
+	n->parent = parent;
+	n->n = value;
+	n->right = NULL;
+	n->left = NULL;
+	return (n);
 }
