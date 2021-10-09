762925c3d005759e54cfd8a15265b9486b1f7ebf
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-09 11:25:59 +0200

    inserted

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -7,16 +7,16 @@
  */
 size_t _depth(const heap_t *tree)
 {
-    size_t d = 0;
+	size_t d = 0;
 
-    if (tree == NULL)
-        return (d);
-    while (tree->parent)
-    {
-        d++;
-        tree = tree->parent;
-    }
-    return (d);
+	if (tree == NULL)
+		return (d);
+	while (tree->parent)
+	{
+		d++;
+		tree = tree->parent;
+	}
+	return (d);
 }
 
 /**
@@ -26,11 +26,11 @@ size_t _depth(const heap_t *tree)
  */
 int _is_leaf(const heap_t *n)
 {
-    if (n == NULL)
-        return (0);
-    else if (n->left == NULL && n->right == NULL)
-        return (1);
-    return (0);
+	if (n == NULL)
+		return (0);
+	else if (n->left == NULL && n->right == NULL)
+		return (1);
+	return (0);
 }
 
 /**
@@ -40,15 +40,15 @@ int _is_leaf(const heap_t *n)
  */
 size_t _height(const heap_t *tree)
 {
-    size_t left, right;
-    
-    if (tree == NULL || _is_leaf(tree))
-        return (0);
-    left = _height(tree->left);
-    right = _height(tree->right);
-    if (right <= left)
-        return (left + 1);
-    return (right + 1);
+	size_t left, right;
+		
+	if (tree == NULL || _is_leaf(tree))
+		return (0);
+	left = _height(tree->left);
+	right = _height(tree->right);
+	if (right <= left)
+		return (left + 1);
+	return (right + 1);
 }
 
 /**
@@ -60,24 +60,24 @@ size_t _height(const heap_t *tree)
  */
 binary_tree_t *_insert(binary_tree_t *tree, size_t l, int value)
 {
-    binary_tree_t *le, *ri;
+	binary_tree_t *le, *ri;
 
-    if (tree == NULL)
-        return (NULL);
-    if (_depth(tree) == l)
-    {
-        if (_is_leaf(tree))
-            return (tree->left = binary_tree_node(tree, value));
-        if (tree->left != NULL && tree->right == NULL)
-            return (tree->right = binary_tree_node(tree, value));
-        return (NULL);
-    }
-    le = _insert(tree->left, l, value);
-    if (le == NULL)
-        ri = _insert(tree->right, l, value);
-    else
-        return (le);
-    return (ri);
+	if (tree == NULL)
+		return (NULL);
+	if (_depth(tree) == l)
+	{
+		if (_is_leaf(tree))
+			return (tree->left = binary_tree_node(tree, value));
+		if (tree->left != NULL && tree->right == NULL)
+			return (tree->right = binary_tree_node(tree, value));
+		return (NULL);
+	}
+	le = _insert(tree->left, l, value);
+	if (le == NULL)
+		ri = _insert(tree->right, l, value);
+	else
+		return (le);
+	return (ri);
 }
 
 /**
@@ -88,27 +88,27 @@ binary_tree_t *_insert(binary_tree_t *tree, size_t l, int value)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    size_t l, i, tmp = 0;
-    heap_t *n = NULL;
+	size_t l, i, tmp = 0;
+	heap_t *n = NULL;
 
-    if (*root == NULL)
-    {
-        *root = binary_tree_node((binary_tree_t *)*root, value);
-        return (*root);
-    }
-    l = _height(*root);
-    for (i = 0; i <= l; i++)
-    {
-        n = (heap_t *)_insert((binary_tree_t *)*root, i, value);
-        if (n != NULL)
-            break;
-    }
-    while (n->parent != NULL && n->n > n->parent->n)
-    {
-        tmp = n->parent->n;
-        n->parent->n = n->n;
-        n->n = tmp;
-        n = n->parent;
-    }
-    return (n);
+	if (*root == NULL)
+	{
+		*root = binary_tree_node((binary_tree_t *)*root, value);
+		return (*root);
+	}
+	l = _height(*root);
+	for (i = 0; i <= l; i++)
+	{
+		n = (heap_t *)_insert((binary_tree_t *)*root, i, value);
+		if (n != NULL)
+			break;
+	}
+	while (n->parent != NULL && n->n > n->parent->n)
+	{
+		tmp = n->parent->n;
+		n->parent->n = n->n;
+		n->n = tmp;
+		n = n->parent;
+	}
+	return (n);
 }
