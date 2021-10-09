552167e96f2bfb8d19962cfd1f6ad9e6b9a1c58a
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-09 10:31:06 +0200

    Create a binary tree

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - create a binary tree node please
+ * @parent: Theparent node of the node to create
+ * @value: the new node value
+ * Return: Binary Tree
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *n;
+
+    n = malloc(sizeof(binary_tree_t));
+    if (n == NULL)
+        return (NULL);
+    n->parent = parent;
+    n->n = value;
+    n->right = NULL;
+    n->left = NULL;
+    return (n);
+}
