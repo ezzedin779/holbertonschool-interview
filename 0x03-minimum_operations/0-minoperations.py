5936fb83c6c5a307aa3a28b27b3d7055629309b0
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-10 18:13:16 +0200

    Minimum ?

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,18 +3,18 @@
 
 
 def minOperations(n):
-	"""Calculate please ?"""
-	result = 0
-	he = 1
-	cal = 1
-	if type(n) is not int or n < 2:
-		return 0
-	while (he != n):
-		if (n % he == 0):
-			cal = he
-			he = 2 * he
-			result += 2
-		else:
-			he = he + cal
-			result += 1
-	return result
+    """Calculate please ?"""
+    result = 0
+    he = 1
+    cal = 1
+    if type(n) is not int or n < 2:
+        return 0
+    while (he != n):
+        if (n % he == 0):
+            cal = he
+            he = 2 * he
+            result += 2
+        else:
+            he = he + cal
+            result += 1
+    return result
