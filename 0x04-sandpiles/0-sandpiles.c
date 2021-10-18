b92b1d2181eb5a450ef55eed79f39b7727b7b2c8
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-18 12:39:58 +0200

    the sandpile sum ?3

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -70,11 +70,11 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 					grid1[i][j] = grid1[i][j] - 4;
 					if (i - 1 >= 0)
 						grid1[i - 1][j]++;
-					if (i + 1 >= 2)
+					if (i + 1 <= 2)
 						grid1[i + 1][j]++;
 					if (j - 1 >= 0)
 						grid1[i][j - 1]++;
-					if (j + 1 >= 0)
+					if (j + 1 <= 2)
 						grid1[i][j + 1]++;
 				}
 			}
