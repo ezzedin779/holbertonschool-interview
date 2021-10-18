ec4c192578b6be30aa1cae3bc6380078271ce550
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-18 13:28:26 +0200

    Parsin ?

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Doing some manipulation for the input"""
+
+
+import sys
+i = 0
+stc = ['200', '301', '400', '401', '403', '404', '405', '500']
+l = 0
+n = [0, 0, 0, 0, 0, 0, 0, 0]
+
+try:
+    for line in sys.stdin:
+        piece = line.split()
+        if len(piece) > 2:
+            if piece[-2] in stc:
+                x = stc.index(piece[-2])
+                n[x] = n[x] + 1
+            i += 1
+            l = l + int(piece[-1])
+        if i == 10:
+            i = 0
+            print("File Size: {}".format(l))
+            for j in range(8):
+                if n[j] != 0:
+                    print("{}: {}".format(stc[j], n[j]))
+except Exception:
+    pass
+finally:
+    print("File size: {}".format(l))
+    for j in range(8):
+        if n[j] != 0:
+            print("{}: {}".format(stc[j], n[j]))
