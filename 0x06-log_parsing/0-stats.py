2a1d2b94da5aac5e09b068c64dfc98caa627910f
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-18 13:41:46 +0200

    Parsin ?

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
 """Doing some manipulation for the input"""
 
+if __name__ == '__main__':
+    import sys
 
-import sys
-i = 0
-stc = ['200', '301', '400', '401', '403', '404', '405', '500']
-l = 0
-n = [0, 0, 0, 0, 0, 0, 0, 0]
+    i = 0
+    stc = ['200', '301', '400', '401', '403', '404', '405', '500']
+    l = 0
+    n = [0, 0, 0, 0, 0, 0, 0, 0]
 
-try:
-    for line in sys.stdin:
-        piece = line.split()
-        if len(piece) > 2:
-            if piece[-2] in stc:
-                j = stc.index(piece[-2])
-                n[j] = n[j] + 1
-            i += 1
-            l = l + int(piece[-1])
-        if i == 10:
-            i = 0
-            print("File size: {}".format(l))
-            for j in range(8):
-                if n[j] != 0:
-                    print("{}: {}".format(stc[j], n[j]))
-except Exception:
-    pass
-finally:
-    print("File size: {}".format(l))
-    for j in range(8):
-        if n[j] != 0:
-            print("{}: {}".format(stc[j], n[j]))
+    try:
+        for line in sys.stdin:
+            piece = line.split()
+            if len(piece) > 2:
+                if piece[-2] in stc:
+                    j = stc.index(piece[-2])
+                    n[j] = n[j] + 1
+                i += 1
+                l = l + int(piece[-1])
+            if i == 10:
+                i = 0
+                print("File size: {}".format(l))
+                for j in range(8):
+                    if n[j] != 0:
+                        print("{}: {}".format(stc[j], n[j]))
+    except Exception:
+        pass
+    finally:
+        print("File size: {}".format(l))
+        for j in range(8):
+            if n[j] != 0:
+                print("{}: {}".format(stc[j], n[j]))
