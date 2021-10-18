5ae4e80a336156dec5215279e8f8a3947411796f
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-18 13:13:53 +0200

    palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,22 +1,5 @@
 #include "lists.h"
 
-/**
- * _length - calculate the length of a linked list
- * @head: the head of the list
- * Return: the length
- */
-int _length(listint_t *head)
-{
-	listint_t *n = head;
-	int len = 1;
-
-	while (n->next != NULL)
-	{
-		len++;
-		n = n->next;
-	}
-	return (len);
-}
 
 /**
  * is_palindrome - check the linked list if it's palindrome ?
@@ -25,30 +8,24 @@ int _length(listint_t *head)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *h = *head;
-	int len = _length(*head);
-	int *new = malloc((len / 2) * sizeof(int));
-	int i, count = 0;
+	listint_t *n, *h;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	
-	for (count = 0; count != len / 2; count++)
-	{
-		new[count] = h->n;
+
+	h = *head;
+	while (h->next)
 		h = h->next;
-	}
-	if (len % 2 != 0)
-		h = h->next;
-	for (i = (len / 2) - 1; i != -1; i--)
+	n = *head;
+	while (h >= n)
 	{
-		if (h->n != new[i])
+		if (n->n == h->n)
 		{
-			free(new);
-			return (0);
+			n = n->next;
+			h -= 2;
 		}
-		h = h->next;
+		else
+			return (0);
 	}
-	free(new);
 	return (1);
 }
