70b7b414a492f76924afccc4ed7927ab20c6a4f3
Abderrahmen Babchia <2150@holbertonschool.com>
2021-12-07 23:40:01 +0100

    API

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,19 +3,19 @@ const request = require('request');
 const process = require('process');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
-request({url: url}, (err1, res, body) => {
+request({ url: url }, (err1, res, body) => {
 	const n = [];
 	const js = JSON.parse(body);
 	js.characters.forEach(urls => {
-		const id = url.split('/')[5];
-		requ({url: url}, (err2, res, body) => {
+		const id = urls.split('/')[5];
+		request({ url: urls }, (err2, res, body) => {
 			const js1 = JSON.parse(body);
 			n[id] = js1.name;
 		});
 	});
 	setTimeout(function (){
-		js.characters.forEach(elem => {
-			const id = elem.split('/')[5];
+		js.characters.forEach(element => {
+			const id = element.split('/')[5];
 			console.log(n[id]);
 		});
 	}, 1000);
