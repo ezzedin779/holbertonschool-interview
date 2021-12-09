68c3ce180ce67c0261a760ab1ea3c3dac2af6665
Abderrahmen Babchia <2150@holbertonschool.com>
2021-12-09 21:31:02 +0100

    get me the list of characters

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,22 +4,22 @@ const process = require('process');
 const Id = process.argv[2];
 const characters = [];
 request({ url: (`https://swapi-api.hbtn.io/api/films/${Id}/`), json: true }, function (e, res, json) {
-	if (e) {
-		return console.log(e);
-	}
-	for (const i of json.characters) {
-		request({ url: i, json: true }, function (err, res, json) {
-			if (err) {
-				return console.log(err);
-			}
-			const id = i.split('/')[5];
-			characters[id] = json.name;
-		}, 2000);
-	}
-	setTimeout(function () {
-		json.characters.forEach(element => {
-			const id = element.split('/')[5];
-			console.log(characters[id]);
-		});
-	}, 2000);
+  if (e) {
+    return console.log(e);
+  }
+  for (const i of json.characters) {
+    request({ url: i, json: true }, function (err, res, json) {
+      if (err) {
+        return console.log(err);
+      }
+      const id = i.split('/')[5];
+      characters[id] = json.name;
+    }, 2000);
+  }
+  setTimeout(function () {
+    json.characters.forEach(element => {
+      const id = element.split('/')[5];
+      console.log(characters[id]);
+    });
+  }, 2000);
 });
