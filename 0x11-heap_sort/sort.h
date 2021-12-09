b8c8f4f0811c2d24654d3f76265f680e523852f6
Abderrahmen Babchia <2150@holbertonschool.com>
2021-12-09 22:12:45 +0100

    HEADER 0.0

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef HEAP_SORT_H
+#define HEAP_SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void star(int *arr, size_t s, int x, size_t count);
+
+#endif
