1fa711f088dc29dfb4d2f9eb892c9d61a1af5ff9
Abderrahmen Babchia <2150@holbertonschool.com>
2021-12-09 22:28:52 +0100

    HEADER 0.0

--- 0x12-advanced_binary_search/search_algos.h
+++ 0x12-advanced_binary_search/search_algos.h
@@ -5,5 +5,7 @@
 #include <stdio.h>
 
 int advanced_binary(int *array, size_t size, int value);
+int search(int *arr, size_t f, size_t l, int s);
+void print_array(int *arr, size_t f, size_t l);
 
 #endif
