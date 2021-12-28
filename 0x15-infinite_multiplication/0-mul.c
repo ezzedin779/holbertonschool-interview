f26130a0f4f576c8a4908047a2d3d886d91f69d1
Abderrahmen Babchia <2150@holbertonschool.com>
2021-12-28 09:42:44 +0100

    INFINITE MUL

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,109 @@
+#include "holberton.h"
+
+/**
+ * length - length of a string
+ * @str: the string to get the length
+ * Return: LENGTH
+ */
+int length(char *str)
+{
+	int res = 0;
+	for (; str[res] != '\0'; res++)
+	;
+
+	return (res);
+}
+
+/**
+ * err - basic error to exist with 98
+ */
+void err(void)
+{
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
+	exit(98);
+}
+
+/**
+ * converter - string to integer
+ * @str: the string to convert
+ * Return: array of the string
+ */
+int *converter(char *str)
+{
+	int i;
+	int y;
+	int *num = NULL;
+	(void) y;
+
+	if (str[0] == '0')
+	{
+		_putchar('0');
+		_putchar('\n');
+		exit(0);
+	}
+	num = malloc(sizeof(int) * length(str));
+	if (num == NULL)
+		err();
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((int)str[i] > 58 || (int)str[i] < 48)
+			err();
+		num[i] = str[i] - '0';
+	}
+	return (num);
+}
+
+/**
+ * main - the main prog
+ * @argv: the arguments
+ * @argc: number of arguments
+ * Return: 0 or 98
+ */
+int main(int argc, char **argv)
+{
+	int *x = NULL, *y = NULL, *prod = NULL, len_1, len_2;
+	int new, carr;
+	int i, j;
+	int b_i = 0, b_j;
+
+	if (argc != 3)
+		err();
+	x = converter(argv[1]);
+	y = converter(argv[2]);
+	len_1 = length(argv[1]);
+	len_2 = length(argv[2]);
+	prod = malloc(sizeof(int) * (len_2 + len_1));
+	if (prod == NULL)
+		err();
+	for (i = len_1 - 1; i >= 0; i--)
+	{
+		carr = 0;
+		b_j = 0;
+		for (j = len_2 - 1; j >= 0; j--)
+		{
+			new = x[i] * y[j] + prod[b_i + b_j] + carr;
+			carr = new / 10;
+			prod[b_j + b_j] = new % 10;
+			b_j++;
+		}
+		if (carr > 0)
+			prod[b_i + b_j] += carr;
+		b_i++;
+	}
+	for (i = len_1 + len_2 - 1; i >= 0; i--)
+	{
+		if (i == len_1 + len_2 - 1 && prod[i] == 0)
+			continue;
+		_putchar(prod[i] + '0');
+	}
+	_putchar('\n');
+	free(x);
+	free(y);
+	free(prod);
+	return (0);
+}
