114597ab304cc24f75c010ff8646d0015520a36f
Abderrahmen Babchia <2150@holbertonschool.com>
2022-01-07 12:56:00 +0100

    Rotated ?

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,10 +5,10 @@
 def rotate_2d_matrix(matrix):
     """rotate the matrix"""
     length = len(matrix)
-    for i in range (length // 2):
-        for j in range(i, length - 1):
+    for i in range(length // 2):
+        for j in range(i, length - i - 1):
             tmp = matrix[i][j]
             matrix[i][j] = matrix[length - 1 - j][i]
             matrix[length - 1 - j][i] = matrix[length - 1 - i][length - 1 - j]
-            matrix[length - 1 -i][length - 1 - j] = matrix[j][length - 1 - i]
+            matrix[length - 1 - i][length - 1 - j] = matrix[j][length - 1 - i]
             matrix[j][length - 1 - i] = tmp
