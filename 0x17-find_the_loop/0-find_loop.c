fc1acfe90f6a3d593e39f4845a3426a78e6cc7ed
Abderrahmen Babchia <2150@holbertonschool.com>
2022-01-12 09:11:48 +0100

    Where are you loop ?

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+
+/**
+ * find_listint_loop - finding the loop
+ * @head: the head of the list to be searched
+ * Return: the adress of the begining or nothing
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tmp, *p;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	p = head->next;
+	tmp = head->next->next;
+	while (tmp && tmp->next)
+	{
+		if (p == fast)
+			break;
+		p = p->next;
+		tmp = tmp->next->next;
+	}
+	if (p != tmp)
+		return (NULL);
+	p = head;
+	while (p != tmp)
+	{
+		p = p->next;
+		tmp = tmp->next;
+	}
+	return (p);
+}
