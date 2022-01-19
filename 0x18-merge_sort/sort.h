2ccd90a7d5fb5fabb4c2943382e90314e8a8cc95
Abderrahmen Babchia <2150@holbertonschool.com>
2022-01-19 08:32:14 +0100

    HEADER 0.2

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -6,6 +6,8 @@
 
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
-
+void top_merge(int *arr, int fir, int mid, int la, int *narr);
+void top_split(int *narr, int fir, int la, int *arr);
+void top_sort(int *arr, int *narr, int size);
 
 #endif
