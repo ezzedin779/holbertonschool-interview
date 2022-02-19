a1ca23e4dd5d0b145a6647936971537e85d07142
Abderrahmen Babchia <2150@holbertonschool.com>
2022-02-19 13:02:57 +0100

    HEADER 0.01

---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int Big(int *arr, size_t size);
+void radix_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif
