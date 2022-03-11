16dc6a6f29fed81699cf2f4dfd325f0eac5c969e
Abderrahmen Babchia <2150@holbertonschool.com>
2022-03-11 10:36:32 +0100

    done ?

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -9,9 +9,9 @@
 int wildcmp(char *s1, char *s2)
 {
 	if (s1 == NULL || s2 == NULL)
-		return(0);
+		return (0);
 	if (*s1 == '\0' && *s2 == '\0')
-		return(1);
+		return (1);
 
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
