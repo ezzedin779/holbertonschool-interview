134310ed840ef178d052a8fd3fc025d5d66d8b5b
Abderrahmen Babchia <2150@holbertonschool.com>
2022-03-16 07:59:17 +0100

    TASK DONE ?

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/pjthon3
+"""
+Creating Pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+	"""Generate pascal Triangle
+		@n: Triangle Size"""
+	m = []
+	c = []
+	for i in range(1, n + 1):
+		for j in range(i):
+			if (j == i - 1 or j == 0):
+				c.append(i)
+			else:
+				c.append(m[i - 2][j - i + 1] + m[i - 2][j - i])
+		m.append(c)
+		c = []
+	return(m)
---
+++ 0x1F-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+List of integers that represent the pascal triangle of an integer
