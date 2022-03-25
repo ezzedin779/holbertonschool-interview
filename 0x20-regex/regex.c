99ab64778ad8964c9cd5e766bd1513fe7f8dc238
Abderrahmen Babchia <2150@holbertonschool.com>
2022-03-25 12:00:07 +0100

    the function?

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -17,15 +17,25 @@ int regex_match(char const *str, char const *pattern)
 	if (*(pattern + 1) == '*')
 	{
 		if (*(pattern) == '.')
+		{
 			return (regex_match(str, pattern + 2) || regex_match(str + 1, pattern));
+		}
 		if (*(pattern) != *str)
+		{
 			return (regex_match(str, pattern + 2));
+		}
 		if (*(pattern) == *str && *(str + 1) == *str)
+		{
 			return (regex_match(str + 1, pattern));
+		}
 		if (*(str + 1) != *str)
+		{
 			return (regex_match(str + 1, pattern + 2));
+		}
 	}
 	if (*pattern == *str || *pattern == '.')
+	{
 		return (regex_match(str + 1, pattern + 1));
+	}
 	return (0);
 }
