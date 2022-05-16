391ee58dcc5d596a48b7dc8170a9b0d2e24e51c5
Abderrahmen Babchia <2150@holbertonschool.com>
2022-05-16 11:32:09 +0200

    Done ?

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,9 +1,38 @@
 #!/usr/bin/python3
-"""Doing some manipulation for the input"""
+"""Compute metrics"""
+import sys
 
-if __name__ == '__main__':
-    import sys
 
-    def _print(stc, fs):
-        """printing the needed information"""
-        
+i = 0
+size = 0
+status = []
+_sorted = {}
+
+try:
+    for line in sys.stdin:
+        split = line.split(" ")
+        f_size = split[-1]
+
+        size += int(f_size)
+
+        status_code = split[-2]
+        status.append(int(status_code))
+        status.sort
+        dictionary = dict((i, status.count(i))
+                            for i in status)
+        dictionary_it = dictionary.items()
+        _sorted = sorted(dictionary_it)
+
+        i = i + 1
+        if (i == 10):
+            print("File size: {:d}".format(size))
+            for k, v in sorted:
+                print("{:d}: {:d}".format(k, v))
+            i = 0
+except Exception:
+    pass
+
+finally:
+    print("File size: {:d}".format(size))
+    for k, v in sorted:
+        print("{:d}: {:d}".format(k, v))
